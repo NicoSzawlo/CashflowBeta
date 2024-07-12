023ba5755d07a6b9feed56f69394b4d23c758d2a
Nico Szawlowski <nico.szawlowski@gmail.com>
2024-07-12 16:52:18 -0600

    In this commit i added: - more mockdata for trx view - a second data grid to display all transactions with this partner - databindings fixed

--- CashflowBeta/ViewModels/TransactionViewModel.cs
+++ CashflowBeta/ViewModels/TransactionViewModel.cs
@@ -5,13 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CashflowBeta.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CashflowBeta.ViewModels
 {
-    public class TransactionViewModel : ViewModelBase
+    public partial class TransactionViewModel : ViewModelBase
     {
-
         public ObservableCollection<CurrencyTransaction> Transactions { get; } = new ObservableCollection<CurrencyTransaction>();
+        public ObservableCollection<CurrencyTransaction> TransactionsWithPartner { get; } = new ObservableCollection<CurrencyTransaction>();
+
+        [ObservableProperty]
+        private CurrencyTransaction _selectedTransaction;
+
+        partial void OnSelectedTransactionChanged(CurrencyTransaction? oldValue, CurrencyTransaction newValue)
+        {
+            TransactionsWithPartner.Clear();
+            if (SelectedTransaction != null)
+            {
+                var partner = newValue.TransactionPartner;
+                var transactionsWithPartner = Transactions
+                    .Where(t => t.TransactionPartner.ID == partner.ID)
+                    .ToList();
+                foreach (var transaction in transactionsWithPartner)
+                {
+                    TransactionsWithPartner.Add(transaction);
+                }
+            }
+        }
 
         public TransactionViewModel()
         {
@@ -48,13 +69,84 @@ namespace CashflowBeta.ViewModels
                 DateTime = new DateTime(2023, 6, 1, 14, 30, 0),
                 Amount = 20.00m,
                 Currency = "EUR",
-                Info = "Payment for services",
-                Reference = "REF001",
+                Info = "Payment for cash oida",
+                Reference = "REF991",
                 Account = Demoaccount,
                 TransactionPartner = Demopartner
             };
+            // New Partner
+            TransactionPartner Newpartner = new TransactionPartner()
+            {
+                ID = 3,
+                Name = "John Doe",
+                AccountIdentifier = "TPB123456789",
+                BankIdentifier = "TPBANK003",
+                Bankcode = "TPCODE003"
+            };
+
+            // New Transaction with Newpartner
+            CurrencyTransaction Newtransaction = new CurrencyTransaction()
+            {
+                ID = 3,
+                DateTime = new DateTime(2023, 6, 5, 10, 0, 0),
+                Amount = 500.00m,
+                Currency = "GBP",
+                Info = "Payment for goods",
+                Reference = "REF003",
+                Account = Demoaccount,
+                TransactionPartner = Newpartner
+            };
+
+            // Another Partner
+            TransactionPartner Anotherpartner = new TransactionPartner()
+            {
+                ID = 4,
+                Name = "Jane Smith",
+                AccountIdentifier = "TPB901234567",
+                BankIdentifier = "TPBANK004",
+                Bankcode = "TPCODE004"
+            };
+
+            // Another Transaction with Anotherpartner
+            CurrencyTransaction Anothertransaction = new CurrencyTransaction()
+            {
+                ID = 4,
+                DateTime = new DateTime(2023, 6, 10, 12, 0, 0),
+                Amount = 100.00m,
+                Currency = "USD",
+                Info = "Payment for services",
+                Reference = "REF004",
+                Account = Demoaccount,
+                TransactionPartner = Anotherpartner
+            };
+
+            // Yet Another Partner
+            TransactionPartner Yetanotherpartner = new TransactionPartner()
+            {
+                ID = 5,
+                Name = "Bob Johnson",
+                AccountIdentifier = "TPB111111111",
+                BankIdentifier = "TPBANK005",
+                Bankcode = "TPCODE005"
+            };
+
+            // Yet Another Transaction with Yetanotherpartner
+            CurrencyTransaction Yetanothertransaction = new CurrencyTransaction()
+            {
+                ID = 5,
+                DateTime = new DateTime(2023, 6, 15, 14, 0, 0),
+                Amount = 300.00m,
+                Currency = "EUR",
+                Info = "Payment for goods",
+                Reference = "REF005",
+                Account = Demoaccount,
+                TransactionPartner = Yetanotherpartner
+            };
             Transactions.Add(Demotransaction);
             Transactions.Add(Demotransaction2);
+            Transactions.Add(Newtransaction);
+            Transactions.Add(Anothertransaction);
+            Transactions.Add(Yetanothertransaction);
         }
     }
 }
